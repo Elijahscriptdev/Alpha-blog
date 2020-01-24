2d98aef4bdf7edfcf2a448211a3453f10479061b
Elijahscriptdev <elijahobominuru@gmail.com>
2020-01-24 15:00:19 +0100

    styled list of articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 7, maximum: 350 }
+  validates :description, presence: true, length: { minimum: 7, maximum: 1350 }
 end
