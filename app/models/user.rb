c515742d4c37887a1eec124872a17c57e4ec9696
Elijahscriptdev <elijahobominuru@gmail.com>
2020-01-25 17:02:41 +0100

    signup users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ActiveRecord::Base
     validates :email, presence: true,
               uniqueness: { case_sensitive: false },
               length: { maximum: 25 },
-              format: { with: VALID_EMAIL_REGEX }      
+              format: { with: VALID_EMAIL_REGEX }
+    has_secure_password      
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_180125) do
+ActiveRecord::Schema.define(version: 2020_01_25_085556) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_24_180125) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
