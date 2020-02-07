63911ad1486da327b0aa77a3723a5e8486ffda62
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-07 03:30:32 +0100

    add admin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
         end
 
         def require_same_user
-            if current_user != @article.user
+            if current_user != @article.user and !current_user.admin?
                 flash[:danger] = "You can only delete your own article"
             redirect_to root_path
             end
