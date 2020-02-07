03bf934ba1b07295fd5c1b1b147d525b855dd0d8
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-07 04:10:19 +0100

    enabled admin to edit delete update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,10 +38,24 @@ class UsersController < ApplicationController
         @user_articles =@user.articles.paginate(page: params[:page], per_page: 5)
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and Article was successfully deleted"
+        redirect_to users_path
+    end
+
     private
 
     def set_user
          @user = User.find(params[:id])
+    end
+
+    def require_admin
+        if current_user != @user and !current_user.admin?
+            flash[:danger] = "Only admins canperform that action"
+        redirect_to root_path
+        end
     end
 
     def user_params
@@ -48,9 +63,9 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-            if current_user != @user
-                flash[:danger] = "You can only edit your own information"
-            redirect_to root_path
-            end
+        if current_user != @user and !current_user.admin?
+            flash[:danger] = "You can only edit your own information"
+        redirect_to root_path
         end
+    end
 end
